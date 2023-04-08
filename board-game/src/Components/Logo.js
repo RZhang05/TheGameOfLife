6b8b7dd08e7b84d22be6cb9c081785fea1098a60
SlowestLoris <zhangraymond05@gmail.com>
2023-04-08 16:03:58 -0400

    Modify some events, change logo, add rules

--- board-game/src/Components/Logo.js
+++ board-game/src/Components/Logo.js
@@ -4,7 +4,7 @@ import styles from "../styles/logo.module.css";
 const Logo = () => {
   return (
     <div className={styles.logoContainer}>
-      <div className={styles.logoText}>The Game Of Life</div>
+      <div className={styles.logoText}></div>
     </div>
   );
 };
--- board-game/src/Components/PopUp.js
+++ board-game/src/Components/PopUp.js
@@ -21,9 +21,45 @@ const PopUp = ({ onClick, title, text, isEvent, ans, options, outcomes }) => {
           {text && !showAns && <div>{text}</div>}
           {!text && (
             <ol>
-              <li>Rule 1</li>
-              <li>Rule 2</li>
-              <li>Rule 3</li>
+              <li>Every player starts on the start cell.</li>
+              <li>
+                Every player rolls a 4-sided fair die to determine how many
+                cells to move forward.
+              </li>
+              <li>
+                The two main stats for a player are money and mental fortitude.
+                Money allows you to get out in certain situations. Mental
+                fortitude allows you to make tough decisions.
+                <ol>
+                  <li>Every player starts off with 1000 money</li>
+                  <li>
+                    There are certain cells that allow players to collect money.
+                    When they land on a money cell, they roll a four-sided die
+                    and multiply the result with 2000. That result (d4 * 2000)
+                    is the amount of money they receive.
+                  </li>
+                  <li>Every player starts off with 5 Mental Fortitude</li>
+                  <li>
+                    On cells with grit, your four-sided dice roll times 2 is the
+                    amount of mental fortitude you gain.
+                  </li>
+                  <li>
+                    If the player does not have enough mental fortitude to
+                    select any options in an event then they must roll a dice to
+                    select.
+                  </li>
+                </ol>
+              </li>
+              <li>
+                Each cell with text represents an interaction. Event cells have
+                options with consequences.
+              </li>
+              <li>
+                You finish the game when you reach the end. However to win you
+                must have the most money and mental fortitude out of all
+                players. If nobody has both conditions then the player who
+                reached the finish first wins.
+              </li>
             </ol>
           )}
           {options && !showAns && (
